ce4c026ac688ac8d8f869d124e1c974512e89e22
PauliVr <paulette.vr.10@gmail.com>
2022-08-16 00:19:07 -0500

    Creates the Result component and add styles with emotion styled

---
+++ src/Components/Resultado.jsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  color: #fff;
+  font-family: 'Lato', sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+const Texto = styled.p`
+  font-size: 18px;
+`;
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`;
+const Resultado = ({ cotizacion }) => {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = cotizacion;
+  return (
+    <Contenedor>
+      <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt='Imagen crypto' />
+      <div>
+        <Precio>
+          El precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          El precio más alto del día: <span>{HIGHDAY}</span>
+        </Texto>
+
+        <Texto>
+          El precio más bajo del día: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Última actualización: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Contenedor>
+  );
+};
+
+export default Resultado;
